986b83002b49f79428c7dc653ab8d229ef852525
OmarHisham99 <ohisham413@gmail.com>
2024-07-12 00:09:20 +0300

    feat: Add ConfigProvider for language direction in App component
    This commit adds the ConfigProvider component from the antd library to the App component. The ConfigProvider is used to set the language direction (ltr or rtl) based on the selected language. This change ensures that the application's layout and components are displayed correctly for different languages.
    
    Addresses task:
    - Add ConfigProvider for language direction in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import { useEffect } from "react";
 import TrackingDetails from "./components/TrackingDetails";
 import ShipmentTimeline from "./components/ShipmentTimeline";
-import { Col, Row } from "antd";
+import { Col, ConfigProvider, Row } from "antd";
 import ShipmentAddress from "./components/ShipmentAddress";
 import { useAppSelector } from "./store/hooks/hooks";
 
@@ -20,24 +20,26 @@ function App() {
   }, [i18n.language]);
 
   return (
-    <div className="App">
-      <Header />
-      <main className="main-content">
-        <TrackingDetails />
-        <Row gutter={[16, 16]} justify="center" className="width-max">
-          <Col xs={24} sm={24} md={16} lg={16} xl={16}>
-            <div className="shipmentTimeline">
-              <ShipmentTimeline />
-            </div>
-          </Col>
-          <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-            <div className="shipmentFeedback">
-              <ShipmentAddress />
-            </div>
-          </Col>
-        </Row>
-      </main>
-    </div>
+    <ConfigProvider direction={i18n.language === "ar" ? "rtl" : "ltr"}>
+      <div className="App">
+        <Header />
+        <main className="main-content">
+          <TrackingDetails />
+          <Row gutter={[16, 16]} justify="center" className="width-max">
+            <Col xs={24} sm={24} md={16} lg={16} xl={16}>
+              <div className="shipmentTimeline">
+                <ShipmentTimeline />
+              </div>
+            </Col>
+            <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+              <div className="shipmentFeedback">
+                <ShipmentAddress />
+              </div>
+            </Col>
+          </Row>
+        </main>
+      </div>
+    </ConfigProvider>
   );
 }
 
